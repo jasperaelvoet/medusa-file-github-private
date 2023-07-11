b1dfa27aeba3befd0fdea4b64cc3f86215e0812b
Jasper Aelvoet <jasper.aelvoet@me.com>
2023-07-11 16:31:24 +0200

    File upload to github

--- src/services/file-storage.ts
+++ src/services/file-storage.ts
@@ -6,24 +6,31 @@ import {
   FileServiceGetUploadStreamResult,
   GetUploadedFileType,
 } from "@medusajs/medusa";
+import { Octokit } from "octokit";
 import * as fs from "fs";
 import path from "path";
 
 class GithubPrivateFileService extends AbstractFileService {
+  client_: Octokit;
+
   private readonly path: string;
-  private readonly github_token: string;
+  private readonly repo: string;
 
   constructor(container, options) {
     super(container);
 
     console.log(options);
 
-    if (!options.path || !options.github_token) {
+    if (!options.path || !options.github_token || !options.repo) {
       throw new Error("Invalid medusa-file-github-private config");
     }
 
+    this.client_ = new Octokit({
+      auth: options.github_token,
+    });
+
     this.path = options.path;
-    this.github_token = options.github_token;
+    this.repo = options.repo;
 
     if (!fs.existsSync(this.path)) {
       fs.mkdirSync(this.path);
@@ -31,6 +38,15 @@ class GithubPrivateFileService extends AbstractFileService {
   }
 
   async upload(file: Express.Multer.File): Promise<FileServiceUploadResult> {
+    const base64File = fs.readFileSync(file.path, { encoding: "base64" });
+
+    this.client_.request("PUT /repos/{repo}/contents/{path}", {
+      repo: this.repo,
+      path: `${this.path}/${file.filename + path.extname(file.originalname)}`,
+      message: "Upload file",
+      content: base64File,
+    });
+
     return new Promise((resolve, reject) => {
       fs.copyFile(
         file.path,
